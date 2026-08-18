c5f5fdcab07a02ac9f38cd58ea80f3b06f12ca4b
sunshinekitsune <xsku@protonmail.com>
2026-08-18 14:20:18 -0500

    Fix resource packs not applying on start

--- src/main/java/com/gtnewhorizon/gtnhlib/mixins/early/models/MixinFileResourcePack.java
+++ src/main/java/com/gtnewhorizon/gtnhlib/mixins/early/models/MixinFileResourcePack.java
@@ -95,26 +95,27 @@ public abstract class MixinFileResourcePack extends AbstractResourcePack impleme
         // Return the default for the metadata, or getting it below will infinitely recurse.
         if (original || METADATA_FILE.equals(resource)) return original;
 
+        if (!resource.endsWith(".json")) return false; // not a JSON model/blockstate
+        var pathParts = resource.split("/");
+        if (pathParts.length < 4) return false; // too short to be a blockstate or model
+        if (!"assets".equals(pathParts[0])) return false;
+        if (!"blockstates".equals(pathParts[2])) return false;
+
         // This song and dance is needed because some calls to getInputStreamByName expect it to never fail,
         // since they're referring to resources bundled in jars. So while the original has a checked exception, we can't
         // rely on everyone checking it.
         final int packFormat;
         IMetadataSection packFormatSection = null;
         try {
-            packFormatSection = getPackMetadata(
-                    Minecraft.getMinecraft().getResourcePackRepository().rprMetadataSerializer,
-                    "pack");
-        } catch (IOException e) {
+            var repo = Minecraft.getMinecraft().getResourcePackRepository();
+            if (repo != null) {
+                packFormatSection = getPackMetadata(repo.rprMetadataSerializer, "pack");
+            }
+        } catch (Exception e) {
             // If the above fails, pack metadata is null and thus format is assumed to be 1 (i.e. old).
         }
         if (!(packFormatSection instanceof PackMetadataSection metadata)) packFormat = 1;
         else packFormat = metadata.getPackFormat();
-
-        if (!resource.endsWith(".json")) return false; // not a JSON model/blockstate
-        var pathParts = resource.split("/");
-        if (pathParts.length < 4) return false; // too short to be a blockstate or model
-        if (!"assets".equals(pathParts[0])) return false;
-        if (!"blockstates".equals(pathParts[2])) return false;
 
         // This is a blockstate, reject it if it's too old.
         return packFormat < PACK_FORMAT_MC_13_X;
